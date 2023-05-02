99ab611efee587e68771876de3a0ea362317876e
oliverdn95 <daniloelsword@gmail.com>
2023-05-02 13:52:57 -0300

    Concluido Task FE-9 - Inserir Página Novo Serviço

---
+++ src/pages/NovoServico/NovoServico.jsx
@@ -0,0 +1,46 @@
+import axios from "axios";
+import { Button, Form } from "react-bootstrap";
+import { useForm } from "react-hook-form";
+import { toast } from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
+
+export function NovoServico() {
+
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const navigate = useNavigate();
+
+    function onSubmit(data) {
+        axios.post("http://localhost:3001/servicos", data)
+            .then(response => {
+                toast.success("Servico adicionado.", { position: "bottom-right", duration: 2000 });
+                navigate("/servicos");
+            })
+            .catch(error => {
+                toast.error("Algo deu errado.", { position: "bottom-right", duration: 2000 });
+                console.log(error);
+            });
+    }
+
+    return (
+        <div className="container">
+            <h1>Novo Serviço</h1>
+            <Form onSubmit={handleSubmit(onSubmit)}>
+                <Form.Group className="mb-3">
+                    <Form.Label>Nome</Form.Label>
+                    <Form.Control type="text" className={errors.nome && "is-invalid"} {...register("nome", { required: "O nome é obrigatório." })} />
+                    {errors.nome && <Form.Text className="invalid-feedback">{errors.nome.message}</Form.Text>}
+                </Form.Group>
+
+                <Form.Group className="mb-3">
+                    <Form.Label>Preco</Form.Label>
+                    <Form.Control type="number" className={errors.preco && "is-invalid"} {...register("preco", { required: "O preco é obrigatório.", min: { value: 1, message: "O valor tem que ser maior que zero"} })} />
+                    {errors.preco && <Form.Text className="invalid-feedback">{errors.preco.message}</Form.Text>}
+                </Form.Group>
+
+                <Button variant="primary" type="submit">
+                    Cadastrar
+                </Button>
+            </Form>
+        </div>
+    );
+}
